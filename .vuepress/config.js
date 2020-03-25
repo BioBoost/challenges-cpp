36c1446c287e02c0c0ce78ce696931271bb7efb3
Nico De Witte <nico.dewitte@outlook.com>
2020-03-25 09:03:37 +0100

    Add challenges vuepress theme

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Challenges for C++ Course',
+  description: "Challenges for the C++ Course of VIVES University College in Bruges",
+  theme: 'vuepress-theme-challenges',
+  themeConfig: {
+    docsDir: '.',
+    repo: 'BioBoost/challenges-cpp',
+    sidebar: [
+      '/'
+    ],
+    nav: [
+      {text: 'VIVES', link: 'https://www.vives.be'},
+    ]
+  }
+}
